9afbf3def67c3d643f2e94d33289b75822c4abda
kajarosz <karolinaannajarosz@gmail.com>
2022-06-02 16:47:41 +0200

    New functionality: deleting actions for current interval. Improvements: spit code into smaller functions.

--- flask_app/main/functions.py
+++ flask_app/main/functions.py
@@ -1,4 +1,7 @@
 from hashlib import md5
+from datetime import datetime, timedelta
+from .exceptions import DatabaseQueryException
+from .models import Action
 import re
 
 MD5_SECRET = 'healthy habits are awesome'
@@ -24,3 +27,81 @@ def is_pass_valid(password):
       return True
     else:
       return False
+
+def current_dashboard(user):
+  habits = user.habits
+  daily_habits = []
+  weekly_habits = []
+  monthly_habits = []
+  now = datetime.utcnow()
+  day_from, day_to = day_interval(now)
+  week_from, week_to = week_interval(now)
+  month_from, month_to = month_interval(now)
+  for habit in habits:
+      habit_json = habit.json()
+      actions = habit.actions
+      if habit_json['recurrence'] == 'DAILY':
+        habit_json = no_of_actions(habit_json, actions, day_from, day_to)
+        daily_habits.append(habit_json)
+      elif habit_json['recurrence'] == 'WEEKLY':
+        habit_json = no_of_actions(habit_json, actions, week_from, week_to)
+        weekly_habits.append(habit_json)
+      elif habit_json['recurrence'] == 'MONTHLY':
+        habit_json = no_of_actions(habit_json, actions, month_from, month_to)
+        monthly_habits.append(habit_json)
+  result = {'user': user.id,
+    'daily_habits': daily_habits,
+    'weekly_habits': weekly_habits,
+    'monthly_habits': monthly_habits}
+  return result
+
+def no_of_actions(habit_json, actions, date_from, date_to):
+  actions = list(filter(lambda Action: date_from <= Action.timestamp < date_to, actions))
+  actions_no = len(actions)
+  habit_json.update({'completed': actions_no})
+  return habit_json
+
+def actions_from_current_interval(user, habit):
+  now = datetime.utcnow()
+  habit_json = habit.json()
+  if habit_json['recurrence'] == 'DAILY':
+    date_from, date_to = day_interval(now)
+  elif habit_json['recurrence'] == 'WEEKLY':
+    date_from, date_to = week_interval(now)
+  elif habit_json['recurrence'] == 'MONTHLY':
+    date_from, date_to = month_interval(now)
+  try:
+    actions = Action.query.filter_by(user=user.id, habit=habit.id).order_by(Action.id.desc()).all()
+    actions = list(filter(lambda Action: date_from < Action.timestamp < date_to, actions))
+    if actions:
+      last_action = actions[-1]
+    else:
+      last_action = None
+  except:
+      message = 'Error occured while filtering database.'
+      raise DatabaseQueryException(message)
+  return last_action
+
+def day_interval(time):
+  day_from = time.replace(hour=0, minute=0, second=0, microsecond=0)
+  day_to = day_from + timedelta(hours=23, minutes=59, seconds=59)
+  return day_from, day_to
+
+def week_interval(time):
+  weekday = time.weekday()
+  week_from = time.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=weekday)
+  week_to = week_from + timedelta(days=6)
+  return week_from, week_to
+
+def month_interval(timestamp):
+  month_from = timestamp.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+  if timestamp.month in [1, 3, 5, 7, 8, 10, 12]:
+      month_to = timestamp.replace(day=31, hour=0, minute=0, second=0, microsecond=0)
+  elif timestamp.month in [2, 4, 6, 9, 11]:
+      month_to = timestamp.replace(day=30, hour=0, minute=0, second=0, microsecond=0)
+  else:
+      if timestamp.year % 4 == 0:
+          month_to = timestamp.replace(day=29, hour=0, minute=0, second=0, microsecond=0)
+      else:
+          month_to = timestamp.replace(day=28, hour=0, minute=0, second=0, microsecond=0)
+  return month_from, month_to
--- flask_app/main/views.py
+++ flask_app/main/views.py
@@ -4,7 +4,7 @@ from werkzeug.exceptions import HTTPException
 from flask_login import login_required, current_user
 from main import db
 from .models import Habit, Action
-from datetime import date, datetime, timedelta
+from .functions import current_dashboard, actions_from_current_interval
 
 
 views = Blueprint('views', __name__)
@@ -36,49 +36,7 @@ def api_spec():
 @login_required
 def dashboard():
     user = current_user
-    habit_objects = user.habits
-    daily_habits = []
-    weekly_habits = []
-    monthly_habits = []
-    current_time = datetime.utcnow()
-    current_day = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
-    weekday = current_time.weekday()
-    current_week_from = current_time.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=weekday)
-    current_week_to = current_week_from + timedelta(days=6)
-    current_month_from = current_time.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-    if current_time.month in [1, 3, 5, 7, 8, 10, 12]:
-        current_month_to = current_time.replace(day=31, hour=0, minute=0, second=0, microsecond=0)
-    elif current_time.month in [2, 4, 6, 9, 11]:
-        current_month_to = current_time.replace(day=30, hour=0, minute=0, second=0, microsecond=0)
-    else:
-        if current_time.year % 4 == 0:
-            current_month_to = current_time.replace(day=29, hour=0, minute=0, second=0, microsecond=0)
-        else:
-            current_month_to = current_time.replace(day=28, hour=0, minute=0, second=0, microsecond=0)
-    for habit_object in habit_objects:
-        habit = habit_object.json()
-        if habit['recurrence'] == 'DAILY':
-            actions = habit_object.actions
-            daily_actions = list(filter(lambda Action: Action.timestamp > current_day, actions))
-            actions_no = len(daily_actions)
-            habit.update({'completed': actions_no})
-            daily_habits.append(habit)
-        elif habit['recurrence'] == 'WEEKLY':
-            actions = habit_object.actions
-            weekly_actions = list(filter(lambda Action: current_week_from < Action.timestamp < current_week_to, actions))
-            actions_no = len(weekly_actions)
-            habit.update({'completed': actions_no})
-            weekly_habits.append(habit)
-        elif habit['recurrence'] == 'MONTHLY':
-            actions = habit_object.actions
-            monthly_actions = list(filter(lambda Action: current_month_from < Action.timestamp < current_month_to, actions))
-            actions_no = len(monthly_actions)
-            habit.update({'completed': actions_no})
-            monthly_habits.append(habit)
-    response =  ({'user': user.id,
-        'daily habits': daily_habits,
-        'weekly habits': weekly_habits,
-        'monthly habits': monthly_habits})
+    response = current_dashboard(user)
     return response
 
 
@@ -117,22 +75,36 @@ def add_habit():
         return {'message': 'New habbit added!'}
 
 # add action
-@views.route('/add-action', methods=['POST'])
+@views.route('/action', methods=['POST', 'DELETE'])
 @login_required
-def add_action():
+def action():
+    user = current_user
+    if not request.form.get('classification'):
+        return {'message': 'Habit info is missing'}
+    else:
+        classification = request.form.get('classification')
+    habit = Habit.query.filter_by(user=user.id, classification=classification).first()
     if request.method == 'POST':
-        user = current_user
-        if not request.form.get('classification'):
-            return {'message': 'Habit info is missing'}
-        else:
-            classification = request.form.get('classification')
-        habit = Habit.query.filter_by(user=user.id, classification=classification).first()
         try:
-            new_action = Action(user=user.id, habit=habit.id)
-            db.session.add(new_action)
+            action = Action(user=user.id, habit=habit.id)
+        except:
+            message = 'Error occured whilecreating new action object.'
+            raise DatabaseQueryException(message)
+        try:
+            db.session.add(action)
             db.session.commit()
+            return {'message': 'New action added!'}
         except:
             message = 'Error occured while adding new action to database.'
             raise DatabaseQueryException(message)
-        return {'message': 'New action added!'}
-        
+    elif request.method == 'DELETE':
+        action = actions_from_current_interval(user, habit)
+        if action == None:
+            return {'message': 'No action found for current time period.'}
+        try:
+            db.session.delete(action)
+            db.session.commit()
+            return {'message': 'Action deleted!'}
+        except:
+            message = 'Error occured while deleting action to database.'
+            raise DatabaseQueryException(message)
